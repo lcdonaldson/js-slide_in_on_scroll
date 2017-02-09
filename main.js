3e8cf60162ec3318ae52e437a1da533d742298e7
Levi Donaldson <lcdbacanas@gmail.com>
2017-02-08 22:45:14 -0600

    added project content

---
+++ main.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+
+  //window and animation items
+  var animation_elements = $.find('.animation-element');
+  var web_win = $(window);
+
+  //check to see if any animation containers are currently in view
+  function activate() {
+    //get current window information
+    var win_ht = web_win.height();
+    var win_top = web_win.scrollTop();
+    var win_bottom = (win_top + win_ht);
+
+    //iterate through elements to see if its in view
+    $.each(animation_elements, function() {
+
+      //get the elements information
+      var element = $(this);
+      var element_ht = $(element).outerHeight();
+      var element_top = $(element).offset().top;
+      var element_bottom = (element_top + element_ht);
+
+      //check to see if this current container is visible (its viewable if it exists between the viewable space of the viewport)
+      if ((element_bottom >= win_top) && (element_top <= win_bottom)) {
+        element.addClass('in-view');
+      } else {
+        element.removeClass('in-view');
+      }
+    });
+
+  }
+
+  //on or scroll, detect elements in view
+  $(window).on('scroll resize', function() {
+      activate()
+    })
+    //trigger our scroll event on initial load
+  $(window).trigger('scroll');
+
+});
